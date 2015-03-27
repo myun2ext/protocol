50d3bfc060bf88f5daa9e35713b2e46346cc6f05
myun2ext <myun2@mbm.nifty.com>
2015-03-27 12:12:16 +0900

    Update protocol.hpp

--- protocol.hpp
+++ protocol.hpp
@@ -1,4 +1,16 @@
 #ifndef __MYUN2__PROTOCOL__HPP__
 #define __MYUN2__PROTOCOL__HPP__
 
+#include <string>
+
+namespace myun2
+{
+	namespace protocol
+	{
+		const ::std::string& method(const char* method, const char* protocol_name, const char* version) {
+			return ::std::string(method) + " " + protocol_name + "/" + version;
+		}
+	}
+}
+
 #endif//__MYUN2__PROTOCOL__HPP__
